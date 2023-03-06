d4facf55e25da7e68a5e7a589b3ef1c2b23f8735
milovangudelj <milovan.gudelj@gmail.com>
2023-03-06 20:18:45 +0100

    Ran codemod `built-in-next-font` and removed `@next/font`

--- apps/piscinadiagordo.it/app/page.tsx
+++ apps/piscinadiagordo.it/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { Inter } from "@next/font/google";
+import { Inter } from "next/font/google";
 
 import { Button } from "@piscinadiagordo/ui";
 
