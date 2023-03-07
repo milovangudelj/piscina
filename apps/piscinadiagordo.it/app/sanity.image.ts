00b0ab7acae8edeec7b8fafbc3f93411df32a0cf
milovangudelj <milovan.gudelj@gmail.com>
2023-03-07 02:01:37 +0100

    Removed eslint comment

--- apps/piscinadiagordo.it/app/sanity.image.ts
+++ apps/piscinadiagordo.it/app/sanity.image.ts
@@ -3,7 +3,6 @@ import { dataset, projectId } from "./config";
 
 export const imageBuilder = createImageUrlBuilder({ projectId, dataset });
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const urlForImage = (
 	source: Parameters<(typeof imageBuilder)["image"]>[0]
 ) => imageBuilder.image(source).auto("format").fit("max");
