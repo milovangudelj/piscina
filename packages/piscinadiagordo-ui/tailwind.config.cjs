5718ee29e879b1344f58f80555d752680f2dcf77
milovangudelj <milovan.gudelj@gmail.com>
2023-03-09 03:35:09 +0100

    Changed package type to "module" from "commonjs"

--- packages/piscinadiagordo-ui/tailwind.config.cjs
+++ packages/piscinadiagordo-ui/tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["./src/**/*.{js,ts,jsx,tsx}"],
-	presets: [require("./public/themeConfig")],
+	presets: [require("./public/themeConfig.cjs")],
 	theme: {
 		extend: {},
 	},
